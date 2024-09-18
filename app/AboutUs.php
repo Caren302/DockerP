9518b1eecc847635292b20b738a2aba44fa1f6e6
Caren302 <1721110574@utectulancingo.edu.mx>
2024-09-18 05:15:12 +0000

    Actualización final

--- app/AboutUs.php
+++ app/AboutUs.php
@@ -3,12 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Docker</title>
-    <link rel="stylesheet" href="../AboutUs.css">
+    <title>About Me</title>
+    <link rel="stylesheet" href="AboutUs.css">
 </head>
 <body>
     <header>
-        <h1 class="nombre">Caren Rodríguez</h1>
+        <h1 class="nombre">Caren Rodríguez</h1> 
     </header>
+    
+    <p>
+        Me gustan los gatos, mi color favorito es el negro, amo los atardeceres porque para mí son momentos mágicos y el fénix es mi animal místico favorito.
+    </p>
+    
+    <img class="Imagen" src="https://media.istockphoto.com/id/1421764176/es/vector/abstract-phoenix-bird-vector-illustration.jpg?s=612x612&w=0&k=20&c=Uip2D7kbHeGLBavTnC1ippXJmM6ThaNmRD4HvtGFMAE=" alt="Imagen de un fénix">
 </body>
 </html>
